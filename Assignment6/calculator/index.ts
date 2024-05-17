c10f7a2f65ec49564ffd7b80b8ed7999b2df04ff
Fazan Arshad <faizanarshad0101@gmail.com>
2024-05-18 01:57:06 +0500

    Calculator

---
+++ Assignment6/calculator/index.ts
@@ -0,0 +1,33 @@
+#! /user/bin/env node
+import inquirer from "inquirer"
+const questions = await inquirer.prompt([{
+    type: "input",
+    name: "num1",
+    message: "enter first number",
+    },
+    {
+        type: "input",
+        name: "num2",
+        message: "enter second number",
+    },
+    {
+        type: "list",
+        name: "operator",
+    message: "enter operator",
+    choices:['+','-','*','/']},]);
+
+    function sum(num1:number, num2:number){
+        return Number(num1) + Number(num2);}
+function difference(num1:number, num2:number){
+    return Number(num1) - Number(num2);}
+function product(num1:number, num2:number){
+    return  num1 * num2;
+}
+function division(num1:number, num2:number){
+    return num1 / num2;
+}
+    if (questions.operator == "+"){console.log("sum of two given numbers;",sum(questions.num1,questions.num2));}
+    else if(questions.operator == "-"){console.log("difference of two given numbers:",difference(questions.num1,questions.num2));}
+    else if(questions.operator == "*"){console.log("product of two given numbers:",product(questions.num1,questions.num2))}
+    else if(questions.operator =="/"){console.log("division of two given numbers:",division(questions.num1,questions.num2))}
+    else {console.log("inavlid operator")}
